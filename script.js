828549d0d80152acd155b77bf820278554e692ba
kylem1122 <kylem1122@yahoo.com>
2017-05-31 14:23:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-    setInterval ('cursorAnimation()', 800);
-});
-
-function cursorAnimation() {
-    $('#cursor').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
-}
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
